a6875fa0b7624b1a6b9fe7a6dd8bffae30fef49a
Toine Hartman <tjbhartman@gmail.com>
2020-06-19 12:06:30 +0200

    Set problem-specific parameters

--- benchmark_functions/BiasVarianceTradeoff.h
+++ benchmark_functions/BiasVarianceTradeoff.h
@@ -39,9 +39,8 @@ namespace hicam
 	    ys.setRaw(ys_raw, 50);
 	    ys_ref.setRaw(ys_ref_raw, 50);
 
-      // TODO what's this?
-//      hypervolume_max_f0 = 1.1;
-//      hypervolume_max_f1 = 1.1;
+      hypervolume_max_f0 = 200;
+      hypervolume_max_f1 = 200;
 
     }
     ~BiasVarianceTradeoff_t() {}
@@ -56,17 +55,18 @@ namespace hicam
     // any positive value
     void set_number_of_parameters(size_t & number_of_parameters)
     {
-      this->number_of_parameters = number_of_parameters;
+      this->number_of_parameters = 3 * K; // just ignore the set value
+      number_of_parameters = this->number_of_parameters;
     }
 
     void get_param_bounds(vec_t & lower, vec_t & upper) const
     {
 
       lower.clear();
-      lower.resize(number_of_parameters, 0.0);
+      lower.resize(number_of_parameters, -10);
 
       upper.clear();
-      upper.resize(number_of_parameters, 1.0);
+      upper.resize(number_of_parameters, 10);
 
     }
 
@@ -148,40 +148,6 @@ namespace hicam
     	std::ostringstream ss;
 	    ss << "BiasVarianceTradeoff(K=" << K << ")";
       return ss.str();
-    }
-
-    // compute VTR in terms of the D_{\mathcal{P}_F}\rightarrow\mathcal{S}
-    bool get_pareto_set()
-    {
-
-      size_t pareto_set_size = 5000;
-
-      // generate default front
-      if (pareto_set.size() != pareto_set_size)
-      {
-
-        pareto_set.sols.clear();
-        pareto_set.sols.reserve(pareto_set_size);
-
-        // the front
-        for (size_t i = 0; i < pareto_set_size; ++i)
-        {
-          solution_pt sol = std::make_shared<solution_t>(number_of_parameters, number_of_objectives);
-
-          sol->param.fill(0.0);
-          sol->param[0] = (i / ((double)pareto_set_size - 1.0));
-
-          define_problem_evaluation(*sol); // runs a feval without registering it.
-
-          pareto_set.sols.push_back(sol);
-        }
-
-        igdx_available = true;
-        igd_available = true;
-
-      }
-
-      return true;
     }
 
   };
